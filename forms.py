a2cc17bb294c32f412f227ea58b92d958e19bf70
Filpor <filip.porebinski@o2.pl>
2022-02-10 18:30:24 +0100

    Aplikacja ktworząca listę rzreczy do zrobienia

---
+++ forms.py
@@ -0,0 +1,9 @@
+from email.policy import default
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired
+
+class TodoForm(FlaskForm):
+    title=StringField('title', validators=[DataRequired()])
+    description=TextAreaField('description', validators=[DataRequired()])
+    done=BooleanField('done',false_values=None)
